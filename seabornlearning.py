b796de57db574540b34e64947afb251f1291259b
spancemoon <duan@hust.edu.cn>
2018-05-23 11:44:20 +0800

    Tte free-state hbond plot

--- seabornlearning.py
+++ seabornlearning.py
@@ -1,5 +1,29 @@
+#!/bin/env python
+#-*-encoding:utf-8-*-
 import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+region = [ 'Albania', 'Algeria', 'Angola', 'Argentina', 'Armenia', 'Azerbaijan',
+       'Bahamas', 'Bangladesh', 'Belize', 'Bhutan', 'Bolivia',
+       'Bosnia and Herzegovina', 'Brazil', 'Burkina Faso', 'Burundi',
+       'Cambodia', 'Cameroon', 'Cape Verde', 'Chile', 'China', 'Colombia',
+       'Costa Rica', 'Cote d Ivoire', 'Cuba', 'Cyprus',
+       "Democratic People's Republic of Korea",
+       'Democratic Republic of the Congo', 'Dominican Republic', 'Ecuador',
+       'Egypt', 'El Salvador', 'Equatorial Guinea', 'Ethiopia', 'Fiji',
+       'Gambia', 'Georgia', 'Ghana', 'Guatemala', 'Guyana', 'Honduras'
+]
+
+kind = [ 'Afforestation & reforestation', 'Biofuels', 'Biogas',
+        'Biomass', 'Cement', 'Energy efficiency', 'Fuel switch',
+       'HFC reduction/avoidance', 'Hydro power',
+        'Leak reduction', 'Material use', 'Methane avoidance',             
+       'N2O decomposition', 'Other renewable energies',
+       'PFC reduction and substitution','PV',
+       'SF6 replacement', 'Transportation', 'Waste gas/heat utilization',
+      'Wind power'
+]
+
 
